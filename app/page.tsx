b9b351feef53b1a34ce5181b63aec45fc63318cd
Chaitanya Pawar <chaitanya09pawar@gmail.com>
2026-09-08 14:33:09 +0530

    changed the metadata

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ export default function Home() {
     <main className="site-shell">
       <iframe
         className="site-frame"
-        src="/rothfinder/index.html"
-        title="RothFinder"
+        src="/rothhod/index.html"
+        title="rothhod"
       />
     </main>
   );
